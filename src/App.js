78079e50735c29df9bb77681330ce2ef76e0914b
Caroline Nguyen <caroline.nguyen@outlook.com>
2019-01-06 13:59:47 -0600

    c05 - completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import TaskList from './components/TaskList';
+import TaskHistory from './components/TaskHistory';
 import * as firebase from 'firebase';
 
 // Initialize Firebase
@@ -18,18 +19,30 @@ firebase.initializeApp(config);
 class App extends Component {
   render() {
     return (
-      <div className="container">
+      <Router>
         <div className="App">
-          <header>
-            <h1>Blocitoff</h1>
-          </header>
-          <main>
-            <div className="container">
-              <Route exact path="/" component={() => <TaskList firebase={firebase} />} />
-            </div>
-          </main>
+          <div className="container">
+              <ul className="nav justify-content-end">
+                <li className="nav-item">
+                  <a href="/taskhistory" className="nav-link">Task History </a>
+                </li>
+                <li className="nav-item">
+                  <a href="/tasklist" className="nav-link">Active Tasks </a>
+                </li>
+              </ul>
+            <header>
+              <h1>Blocitoff</h1>
+            </header>
+            <main>
+              <div className="container">
+                <Route exact path="/" component={() => <TaskList firebase={firebase} />} />
+                <Route path="/taskhistory" component={() => <TaskHistory firebase={firebase} />} />
+                <Route path="/tasklist" component={() => <TaskList firebase={firebase} />} />
+              </div>
+            </main>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/components/TaskHistory.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+
+class TaskHistory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [],
+      newTaskName: '',
+      createdAt: '',
+      isCompleted: '',
+    };
+
+    this.tasksRef = this.props.firebase.database().ref('tasks');
+  }
+
+  componentDidMount() {
+    this.tasksRef.on('child_added', snapshot => {
+      const task = snapshot.val();
+      task.key = snapshot.key;
+
+      this.setState({ tasks: this.state.tasks.concat(task).filter(task =>
+        (task.createdAt < (Date.now() - 604800000)) || (task.isCompleted === true)) })
+    });
+  }
+
+  switchComplete(task) {
+    task.isCompleted = !task.isCompleted;
+    this.tasksRef.child(task.key).update({
+      isCompleted: task.isCompleted,
+    });
+
+    this.setState({ tasks: this.state.tasks.filter(task =>
+      (task.createdAt < (Date.now() - 604800000)) || (task.isCompleted === true)) })
+  }
+
+  render() {
+    return (
+      <section className="tasks">
+        <h5>Completed</h5>
+        <ul className="list-group">
+        { this.state.tasks.filter(task => (task.isCompleted === true)).map((task, index) =>
+            <li className="list-group-item" key={ index }><input type="checkbox" onChange={() => this.switchComplete(task)} checked={task.isCompleted}/> { task.name }</li>
+          )
+        }
+        </ul>
+        <br/>
+        <h5>Expired</h5>
+        <ul className="list-group">
+        { this.state.tasks.filter(task => (task.createdAt < (Date.now() - 604800000)) && (task.isCompleted === false)).map((task, index) =>
+            <li className="list-group-item" key={ index }><input type="checkbox" onChange={() => this.switchComplete(task)} checked={task.isCompleted}/> { task.name }</li>
+          )
+        }
+        </ul>
+        <br/>
+      </section>
+    );
+  }
+}
+
+export default TaskHistory;
